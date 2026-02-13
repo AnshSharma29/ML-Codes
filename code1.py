36c76f4e141e8400e0202e771da61939e5198a62
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2026-02-13 23:10:01 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -87,16 +87,20 @@ print(d5)
 # reading the database
 data = pd.read_csv("tips.csv")
 
+
 # Scatter plot with day against tip
 plt.plot(data['tip'])
 plt.plot(data['size'])
 
+
 # Adding Title to the Plot
 plt.title("Scatter Plot")
+
 
 # Setting the X and Y labels
 plt.xlabel('Day')
 plt.ylabel('Tip')
+
 
 plt.show()
 
